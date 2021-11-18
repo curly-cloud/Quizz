ed9559ebbd10dbaea0353d5ff97783b7473f0b05
Siré Sonko <kahddy@gmail.com>
2021-11-18 15:20:38 +0100

    ask a question

--- js/app.js
+++ js/app.js
@@ -1,6 +1,25 @@
 //initialiser le jeu avec une fonction anonyme stocké dans une variable 
 const init = () => {
     console.log("lance le jeu bg");
+    //recupération d'une question 
+    function askQuestion (questionIndex) {
+        question = questions[questionIndex];
+        return question;
+    }
+
+    let questionAsked = askQuestion (0); 
+    console.log(questionAsked);
+    //placer la question dans le DOM
+    function addQuestionToDOM(questionIndex) {
+     //Selection de l'id
+     let questionName = document.querySelector('#question-index');
+     //insertion
+      questionName.innerHTML = questions[questionIndex]; 
+      console.log(questionName );
+    }
+
+     let addedQuestionToDom = addQuestionToDOM(5); 
+    console.log(addedQuestionToDom); 
 }
 
 init();
